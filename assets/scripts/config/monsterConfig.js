2564106432464b9859fe2cc605eaef800f5ba224
ckyrt <463444759@qq.com>
2020-08-20 12:42:39 +0800

    移除不用的怪物图

--- assets/scripts/config/monsterConfig.js
+++ assets/scripts/config/monsterConfig.js
@@ -3,7 +3,7 @@ var monsterConfig = {
     '兽人':{
         'hp':50,
         'max_hp':50,
-        'imgSrc':'monstersavataricons/MonstersAvatarIcons_2',
+        'imgSrc':'monstersavataricons/MonstersAvatarIcons_1',
         'attack':7,
         'defend':10,
         'name':'兽人',
@@ -21,7 +21,7 @@ var monsterConfig = {
     '野猪':{
         'hp':70,
         'max_hp':70,
-        'imgSrc':'monstersavataricons/MonstersAvatarIcons_13',
+        'imgSrc':'monstersavataricons/MonstersAvatarIcons_2',
         'attack':11,
         'defend':15,
         'name':'野猪',
@@ -39,7 +39,7 @@ var monsterConfig = {
     '蜥蜴人':{
         'hp':100,
         'max_hp':100,
-        'imgSrc':'monstersavataricons/MonstersAvatarIcons_7',
+        'imgSrc':'monstersavataricons/MonstersAvatarIcons_3',
         'attack':18,
         'defend':18,
         'name':'蜥蜴人',
@@ -57,7 +57,7 @@ var monsterConfig = {
     '僵尸':{
         'hp':120,
         'max_hp':120,
-        'imgSrc':'monstersavataricons/MonstersAvatarIcons_15',
+        'imgSrc':'monstersavataricons/MonstersAvatarIcons_4',
         'attack':40,
         'defend':30,
         'name':'僵尸',
@@ -75,7 +75,7 @@ var monsterConfig = {
     '骷髅兵':{
         'hp':160,
         'max_hp':160,
-        'imgSrc':'monstersavataricons/MonstersAvatarIcons_11',
+        'imgSrc':'monstersavataricons/MonstersAvatarIcons_5',
         'attack':50,
         'defend':35,
         'name':'骷髅兵',
@@ -94,7 +94,7 @@ var monsterConfig = {
     '烈火鸟':{
         'hp':200,
         'max_hp':200,
-        'imgSrc':'monstersavataricons/MonstersAvatarIcons_1',
+        'imgSrc':'monstersavataricons/MonstersAvatarIcons_6',
         'attack':50,
         'defend':35,
         'name':'烈火鸟',
@@ -113,7 +113,7 @@ var monsterConfig = {
     '食人魔':{
         'hp':250,
         'max_hp':250,
-        'imgSrc':'monstersavataricons/MonstersAvatarIcons_18',
+        'imgSrc':'monstersavataricons/MonstersAvatarIcons_7',
         'attack':70,
         'defend':45,
         'name':'食人魔',
@@ -132,7 +132,7 @@ var monsterConfig = {
     '泰山':{
         'hp':300,
         'max_hp':300,
-        'imgSrc':'monstersavataricons/MonstersAvatarIcons_23',
+        'imgSrc':'monstersavataricons/MonstersAvatarIcons_8',
         'attack':80,
         'defend':50,
         'name':'泰山',
@@ -151,7 +151,7 @@ var monsterConfig = {
     '路西法':{
         'hp':350,
         'max_hp':350,
-        'imgSrc':'monstersavataricons/MonstersAvatarIcons_14',
+        'imgSrc':'monstersavataricons/MonstersAvatarIcons_9',
         'attack':90,
         'defend':50,
         'name':'路西法',
